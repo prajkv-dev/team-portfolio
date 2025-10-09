b9a83c5dfecaaac49e90d159fadc3e18bb041fb4
prajwal kv <kvpraju07@gmail.com>
2025-10-09 19:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const teamMembers = [
+ const teamMembers = [
   {
     name: "Vignesh B.Y",
     role: "Team Leader",
     email: "vigneshshettyby@gmail.com",
-    instagram: "https://www.instagram.com/acosmist.77?utm_source=qr&igsh=cndtODJoaWc1ank3"
+    insta: "https://www.instagram.com/acosmist.77?utm_source=qr&igsh=cndtODJoaWc1ank3"
   },
   {
     name: "Prajwal K.V",
     role: "Team Representative",
     email: "kvpraju07@gmail.com",
-    instagram: "https://www.instagram.com/prajwal_._kv_?igsh=M2lpcHFhbWlsd3E4",
+    insta: "https://www.instagram.com/prajwal_._kv_?igsh=M2lpcHFhbWlsd3E4",
     linkedin: "https://www.linkedin.com/in/prajwal-kv-4b16b4364"
   },
+   {  name: "Subhas ",
+    role: "team member",
+    email: "subworrior@gmail.com",
+    insta:"https://www.instagram.com/sub_worier_128?utm_source=qr&igsh=MTIyM3l3bTVtOXRiOQ=="
+   },
   {
     name: "Kiran Kumar N",
     role: "Team Member",
@@ -25,10 +30,16 @@ const teamMembers = [
     linkedin: "https://www.linkedin.com/in/keerthana-s-660646388"
   },
   {
+    name: "Shravya",
+    role: "Team Member",
+    email: "shravyag30@gmail.com",
+    insta: "https://www.instagram.com/moon.gurl30?utm_source=qr&igsh=MWtkemE5ZnZkcmtqNA=="
+  },
+  {
     name: "M. Deekshitha",
     role: "Team Member",
     email: "dheekshitham7@gmail.com",
-    instagram: "https://www.instagram.com/deekshu._.07?igsh=aXdmdHVxN3Fmbnh0",
+    insta: "https://www.instagram.com/deekshu._.07?igsh=aXdmdHVxN3Fmbnh0",
     linkedin: "https://www.linkedin.com/in/m-dheekshitha-b16503388"
   },
   {
@@ -50,10 +61,10 @@ teamMembers.forEach(member => {
     <p><strong>Role:</strong> ${member.role}</p>
     <p><strong>Email:</strong> <a href="mailto:${member.email}">${member.email}</a></p>
     <div class="social-links">
-      ${member.instagram ? `<a href="${member.instagram}" target="_blank">Instagram</a>` : ""}
+      ${member.insta ? `<a href="${member.insta}" target="_blank">Instagram</a>` : ""}
       ${member.linkedin ? `<a href="${member.linkedin}" target="_blank">LinkedIn</a>` : ""}
     </div>
   `;
 
   container.appendChild(card);
-  }); 
+});
